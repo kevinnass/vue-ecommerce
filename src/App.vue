c8c1508eebbfe2cfeeec1fd9dc9a8cf8a2df66a6
kevinnassara <nassarakevin@gmail.com>
2024-03-18 13:34:25 +0100

    add missing dependencie for deploiement 3

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import GlobalLoader from 'vue-global-loader';
-import CircleSpinner from 'vue-global-loader';
+// import GlobalLoader from 'vue-global-loader';
+// import CircleSpinner from 'vue-global-loader';
 import { Notivue, Notifications } from 'notivue';
 // import Navbar from '@/components/Navbar.vue'
 
 </script>
 
 <template>
-  <GlobalLoader>
+  <!-- <GlobalLoader>
     <CircleSpinner />
-  </GlobalLoader>
+  </GlobalLoader> -->
 
   <!-- <Notivue v-slot="item">
     <Notifications :item="item" />
